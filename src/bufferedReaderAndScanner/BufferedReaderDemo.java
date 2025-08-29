97f2c6895a30673e5faa17d815767409bcdbfa22
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-08-29 14:28:01 +0530

    Added Buffered Reader

---
+++ src/bufferedReaderAndScanner/BufferedReaderDemo.java
@@ -0,0 +1,16 @@
+package bufferedReaderAndScanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderDemo {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+
+        int num = Integer.parseInt(bf.readLine());
+        System.out.println(num);
+    }
+}
