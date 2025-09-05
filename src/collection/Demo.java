1ac87f441f4168c285c781b9e3510fc4100289d0
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-05 19:51:52 +0530

    Added List

---
+++ src/collection/Demo.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.*;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Collection<Integer> numbs = new ArrayList<Integer>();
+        numbs.add(5);
+        numbs.add(3);
+        numbs.add(6);
+        numbs.add(9);
+
+        System.out.println(numbs);
+        System.out.println("--------------------------------");
+
+        for(Integer n : numbs){
+            System.out.println(n);
+        }
+    }
+}
